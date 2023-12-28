29b9560455c7d5222de3303139ba346eae274e6b
Admin <kopylov.a98@mail.ru>
2023-12-28 21:37:24 +0500

    работает

--- consumer/models.py
+++ consumer/models.py
@@ -7,8 +7,14 @@ class ApiUser(models.Model):
     email = models.EmailField()
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
 
 
 
